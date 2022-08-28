568e1d4171ebd51bda40f632b8d76d1ad6eda787
Pablo <dev@pabl.xyz>
2022-08-28 18:34:07 -0500

    Remove initial content

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hello there #FutureShaper...</h3>
-        <p>
-          This is a simple React app that will be used to asses your skills.
-        </p>
-        <p>
-          Please follow the instructions in the README.md file to get started.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and show me what you got!
-        </p>
-      </header>
     </div>
   );
 }
