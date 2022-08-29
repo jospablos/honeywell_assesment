6a710d31093dcb91ee246e597e40f73c8dd9a544
Pablo <dev@pabl.xyz>
2022-08-29 06:48:07 -0500

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import SignupPage from './modules/Signup/SignupPage';
 
+function LandingPage({setShowSignup}) {
+  return (
+    <>
+      <div className='Landing-description'>
+        <h1>Welcome to this demo signup</h1>
+        <p>We hope you enjoy this short flow.</p>
+      </div>
+      <button className='SignupButton' onClick={() => setShowSignup(true)}>Sign up</button>
+    </>
+  );
+}
+
 function App() {
+  const [showSignup, setShowSignup] = useState(false);
+
   return (
     <div className="App">
-      <SignupPage />
+      {showSignup ? <SignupPage /> : <LandingPage setShowSignup={setShowSignup} />}
     </div>
   );
 }
