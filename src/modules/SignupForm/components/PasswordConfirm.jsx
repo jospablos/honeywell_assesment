c32e6836fbf7a200d88e56cca0bc9a0792a36c03
Pablo <dev@pabl.xyz>
2022-08-28 22:37:28 -0500

    Add password confirmation

--- src/modules/SignupForm/components/PasswordConfirm.jsx
+++ src/modules/SignupForm/components/PasswordConfirm.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import DefaultInput from "./DefaultInput";
 
 // TODO: when converted to TS, create a common input interface.
 export default function PasswordConfirm(props) {
-    return (
-        <>
-            <DefaultInput {...props} />
-        </>
-    )
+  const [confirmation, setConfirmation] = useState('');
+  const [confirmationError, setConfirmationError] = useState(null);
+
+  const onConfirmationChange = (_, value) => {
+    setConfirmation(value);
+  };
+
+  const validateConfirmation = (value) => {
+    if (value !== props.value) {
+      return "Passwords do not match";
+    }
+  };
+
+  const onConfirmationFinished = (id, isComplete, error) => {
+    setConfirmationError(error);
+    props.onFinished(props.id, isComplete);
+  }
+
+  return (
+    <>
+      <DefaultInput {...props} />
+      <DefaultInput
+        value={confirmation}
+        label={"Confirm password"}
+        error={confirmationError}
+        onChange={onConfirmationChange}
+        validate={validateConfirmation}
+        onFinished={onConfirmationFinished}
+      />
+    </>
+  );
 }
