0b2d6faf4c8a70ec180b5c781dc2d00ad3a5ee83
Pablo <dev@pabl.xyz>
2022-08-28 21:21:10 -0500

    Add input error state

--- src/modules/SignupForm/components/DefaultInput.jsx
+++ src/modules/SignupForm/components/DefaultInput.jsx
@@ -1,14 +1,16 @@
 import './DefaultInput.css';
+import classnames from 'classnames';
 
-export default function DefaultInput({ id, label, type = 'text', value, onChange: onChangeProp }) {
+export default function DefaultInput({ id, label, error, type = 'text', value, onChange: onChangeProp }) {
     const onChange = (e) => {
         onChangeProp(id, e.target.value);
     }
 
     return (
-        <div className="DefaultInput-container">
+        <div className={classnames("DefaultInput-container", { error })}>
             <label>{label}</label>
             <input value={value} type={type} onChange={onChange} />
+            {error && <div className="DefaultInput-error">{error}</div>}
         </div>
     );
 }
