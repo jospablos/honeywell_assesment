a3354ad59c0fa9bccc0fde039cca1a84c3ffddab
Pablo <dev@pabl.xyz>
2022-09-05 23:16:46 -0500

    Fix typo

--- src/modules/Signup/SuccessStep/SuccessStep.jsx
+++ src/modules/Signup/SuccessStep/SuccessStep.jsx
@@ -3,7 +3,7 @@ import './SuccessStep.css';
 export default function SuccessStep() {
   return (
     <div className="SuccessStep-container Page-full">
-      <h1>Your acount is ready!</h1>
+      <h1>Your account is ready!</h1>
       <button className="PrimaryActionButton" onClick={() => window.location.reload()}>Start</button>
     </div>
   );
